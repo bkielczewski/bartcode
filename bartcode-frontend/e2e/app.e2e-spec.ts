8da30b75df2e223425a1fcb697240650f1f68d42
Bartosz Kielczewski <bartosz@kielczewski.eu>
2017-03-14 23:25:46 +0000

    Added new ng application with material and flex-layout deps

---
+++ bartcode-frontend/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { BartcodeFrontendPage } from './app.po';
+
+describe('bartcode-frontend App', () => {
+  let page: BartcodeFrontendPage;
+
+  beforeEach(() => {
+    page = new BartcodeFrontendPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
