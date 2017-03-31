afa79f8d17e3725be2452afb1397fb56adc241ef
Bartosz Kielczewski <bartosz@kielczewski.eu>
2017-04-01 00:52:39 +0100

    Added highlight.js, for now it's used client-side whenever there's a browser present

--- bartcode-frontend/src/app/document/document.ts
+++ bartcode-frontend/src/app/document/document.ts
@@ -1,11 +1,18 @@
+import { SafeHtml } from '@angular/platform-browser';
+
 export interface Document {
+
   relativeUrl: string;
   metadata: Metadata;
   text: string;
+  textSafeHtml: SafeHtml;
+
 }
 
 export interface Metadata {
+
   canonicalUrl: string;
   title: string;
   description: string;
+
 }
--- bartcode-frontend/src/app/post/post/post.component.ts
+++ bartcode-frontend/src/app/post/post/post.component.ts
@@ -1,21 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Post } from '../post';
+import { DomSanitizer } from '@angular/platform-browser';
+import * as hljs from 'highlight.js';
+
+declare const document;
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent implements OnInit {
-
+export class PostComponent implements OnInit, AfterViewInit {
   post: Post;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,
+              private sanitizer: DomSanitizer) {
   }
 
   ngOnInit() {
-    this.route.data.subscribe((data: { post: Post }) => this.post = data.post);
+    this.route.data.subscribe((data: { post: Post }) => this.onPostLoaded(data.post));
+  }
+
+  onPostLoaded(post: Post) {
+    post.textSafeHtml = this.sanitizer.bypassSecurityTrustHtml(post.text);
+    this.post = post;
+  }
+
+  ngAfterViewInit(): void {
+    if (document) {
+      const blocks = document.querySelectorAll('pre code');
+      Array(blocks).forEach.call(blocks, hljs.highlightBlock);
+    }
   }
 
 }
