6841ec5855ce24c94620a90107f9f77150ccc6e2
Bartosz Kielczewski <bartosz@kielczewski.eu>
2017-03-31 21:29:42 +0100

    Fixed mixed up matchers

--- bartcode-frontend/src/app/post/post-routing-url-matchers.ts
+++ bartcode-frontend/src/app/post/post-routing-url-matchers.ts
@@ -1,11 +1,11 @@
 import { UrlMatchResult } from '@angular/router/src/config';
 import { UrlSegment } from '@angular/router';
 
-export function yearMonth(url: UrlSegment[]): UrlMatchResult {
+export function year(url: UrlSegment[]): UrlMatchResult {
   return (url.length == 1 && /^([0-9]{4,})$/.test(url[0].path)) ? { consumed: url, posParams: { year: url[0] } } : null;
 }
 
-export function year(url: UrlSegment[]): UrlMatchResult {
+export function yearMonth(url: UrlSegment[]): UrlMatchResult {
   return (url.length == 2 && /^([0-9]{4,})$/.test(url[0].path) && /^([0-9]{2,})$/.test(url[1].path)) ?
     { consumed: url, posParams: { year: url[0], month: url[1] } } : null;
 }
