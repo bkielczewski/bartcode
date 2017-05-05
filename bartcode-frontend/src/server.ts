0a20b3dab7482689a02f20e484d402900211c88c
Bartosz Kielczewski <bartosz@kielczewski.eu>
2017-05-05 22:13:52 +0100

    Changed server to handle only paths defined in the application, throw 404 for NotFoundError, and 404 for any unmatched path

--- bartcode-frontend/src/server.ts
+++ bartcode-frontend/src/server.ts
@@ -7,6 +7,7 @@ import * as express from 'express';
 import * as url from 'url';
 import * as compression from 'compression';
 import * as apicache from 'apicache';
+import { NotFoundError } from './app/error/errors';
 
 enableProdMode();
 
@@ -14,38 +15,52 @@ const PORT = process.env.PORT || 4000;
 const dist = join(__dirname, '..', 'dist');
 const documentAssets = join(__dirname, '..', '..', 'data', 'documents', 'assets');
 const postAssets = join(__dirname, '..', '..', 'data', 'posts', 'assets');
+
 const builder = new ApplicationBuilderFromModuleFactory(AppModuleNgFactory, join(dist, 'index.html'));
 const application = builder.build();
-const app = express();
 const cache = apicache.middleware;
+const app = express();
 
 app.use(compression());
 
-app.get('/documents/assets/**', express.static(documentAssets));
-app.get('/posts/assets/**', express.static(postAssets));
-
-app.get('*.*', express.static(dist));
-
-app.get(/.*/, cache('5 minutes'), async (request, response) => {
+const appRouteHandler = async (request, response) => {
   try {
-    const uri = absoluteUri(request);
+    const uri = getAbsoluteUri(request);
     const snapshot = await application.renderUri(uri);
     response.send(snapshot.renderedDocument);
   }
-  catch (exception) {
-    console.error(exception);
-    response.send(builder.templateDocument());
+  catch (error) {
+    if (error instanceof NotFoundError) {
+      response.status(404).send(error.message);
+    } else {
+      console.error(error.message);
+      response.status(500).send(error.message);
+    }
   }
-});
+};
 
-app.listen(PORT, () => {
-  console.log(`listening on http://localhost:${PORT}!`);
-});
-
-const absoluteUri = (request: express.Request): string => {
+const getAbsoluteUri = (request: express.Request): string => {
   return url.format({
     protocol: request.protocol,
     host: request.get('host'),
     pathname: request.originalUrl
   });
 };
+
+const appRoutes: Promise<any> = application.discoverRoutes()
+  .then(routes => routes
+    .filter(route => route.path)
+    .map(route => '/' + route.path.join('/'))
+    .forEach(path => app.get(path, cache('5 minutes'), appRouteHandler))
+  );
+
+app.get('/documents/assets/**', express.static(documentAssets));
+app.get('/posts/assets/**', express.static(postAssets));
+app.get('/*.*', express.static(dist));
+
+Promise.all([appRoutes]).then(() => {
+  app.get('*', (request, response) => response.status(404).send('Page Not Found, url=' + request.originalUrl));
+  app.listen(PORT, () => {
+    console.log(`listening on http://localhost:${PORT}!`);
+  })
+});
