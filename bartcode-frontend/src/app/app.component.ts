d59edcf48fc9528baa81dbd9f77d0952f0e6ba4d
Bartosz Kielczewski <bartosz@kielczewski.eu>
2017-11-09 22:42:29 +0000

    On navigation event scroll to top of the page if in browser

--- bartcode-frontend/src/app/app.component.ts
+++ bartcode-frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { NavigationStart, Router } from '@angular/router';
 import { MetadataService } from './metadata/metadata.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +9,29 @@ import { MetadataService } from './metadata/metadata.service';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private router: Router, private metadataService: MetadataService) {
+  constructor(private router: Router,
+              private metadataService: MetadataService,
+              @Inject(PLATFORM_ID) private platformId: Object) {
   }
 
   ngOnInit(): void {
     this.router.events
       .filter(event => event instanceof NavigationStart)
-      .subscribe((data) => this.metadataService.resetMetadata());
+      .subscribe(() => this.onNavigationStart());
   }
 
+  private onNavigationStart() {
+    this.resetMetadata();
+    this.scrollIntoViewOnBrowser();
+  }
+
+  private resetMetadata() {
+    this.metadataService.resetMetadata();
+  }
+
+  private scrollIntoViewOnBrowser() {
+    if (isPlatformBrowser(this.platformId)) {
+      window.scroll(0, 0);
+    }
+  }
 }
