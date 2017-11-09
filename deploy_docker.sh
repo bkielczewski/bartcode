6953300a24cb5cedcb4b51c026a9e2d89263bb78
Bartosz Kielczewski <bartosz@kielczewski.eu>
2017-11-09 00:11:36 +0000

    Fixed shippable build - building docker images

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -2,31 +2,42 @@
 
 if [ $# -eq 0 ]
 then
-    echo -n "Usage: $0 docker_image_name"
+    echo -n "Usage: $0 docker_image_name [directory where Dockerfile is]"
+    exit 1
 fi
+
+if [ ! -z ${IS_GIT_TAG+x} ] && [ -z ${GIT_TAG_NAME+x} ]
+then
+    echo -n "When IS_GIT_TAG is set, GIT_TAG_NAME should be set too"
+    exit 1
+fi
+
+IMAGE=${1}
+DOCKERFILE_DIR=${2:-./}
+BRANCH=${BRANCH:-master}
+IS_GIT_TAG=${IS_GIT_TAG:-false}
+
+OLD_PWD=$PWD
+cd ${DOCKERFILE_DIR}
 
 if [ ! -f ./Dockerfile ]; then
     echo -n "No Dockerfile found in current working directory."
+    exit 1
 fi
 
-if [ -z ${BRANCH+x} ] || [ -z ${IS_GIT_TAG+x} ] || [ -z ${GIT_TAG_NAME+x} ]
-then
-    echo -n "Environment variables: BRANCH, IS_GIT_TAG, GIT_TAG_NAME are not set."
-fi
-
-IMAGE=$1
-
-docker build -t $IMAGE:$BRANCH .
-docker push $IMAGE:$BRANCH
+docker build -t ${IMAGE}:${BRANCH} .
+docker push ${IMAGE}:${BRANCH}
 
 if [ "$IS_GIT_TAG" == "true" ]
 then
-    docker tag $IMAGE:$BRANCH $IMAGE:$GIT_TAG_NAME
-    docker push $IMAGE:$GIT_TAG_NAME
+    docker tag ${IMAGE}:${BRANCH} ${IMAGE}:$GIT_TAG_NAME
+    docker push ${IMAGE}:${GIT_TAG_NAME}
 fi
 
 if [ "$BRANCH" == "master" ]
 then
-    docker tag $IMAGE:$BRANCH $IMAGE:latest
-    docker push $IMAGE:latest
+    docker tag ${IMAGE}:${BRANCH} ${IMAGE}:latest
+    docker push ${IMAGE}:latest
 fi
+
+cd ${OLD_PWD}
