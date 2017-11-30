89613817fc4f3c59492b6d2ab1a787db432e5af9
Bartosz Kielczewski <bartosz@kielczewski.eu>
2017-11-30 21:54:15 +0000

    Remove redundant @Param

--- bartcode-service/src/main/kotlin/uk/co/bartcode/service/post/PostRepository.kt
+++ bartcode-service/src/main/kotlin/uk/co/bartcode/service/post/PostRepository.kt
@@ -4,30 +4,29 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RestResource
 import java.util.*
 
 interface PostRepository : JpaRepository<Post, Long> {
 
     @RestResource(path = "relativeUrl", rel = "relativeUrl")
-    fun findOneByRelativeUrl(@Param("relativeUrl") relativeUrl: String): Optional<Post>
+    fun findOneByRelativeUrl(relativeUrl: String): Optional<Post>
 
     @RestResource(path = "recent", rel = "recent")
     fun findAllByOrderByPublishedDesc(pageable: Pageable): Page<Post>
 
     @RestResource(path = "year", rel = "year")
     @Query("SELECT p FROM Post p " + "WHERE FUNCTION('YEAR', p.published) = ?1")
-    fun findByYear(@Param("year") year: Int, pageable: Pageable): Page<Post>
+    fun findByYear(year: Int, pageable: Pageable): Page<Post>
 
     @RestResource(path = "yearMonth", rel = "yearMonth")
     @Query("SELECT p FROM Post p " +
             "WHERE FUNCTION('YEAR', p.published) = ?1 " +
             "AND FUNCTION('MONTH', p.published) = ?2")
-    fun findByYearAndMonth(@Param("year") year: Int, @Param("month") month: Int, pageable: Pageable): Page<Post>
+    fun findByYearAndMonth(year: Int, month: Int, pageable: Pageable): Page<Post>
 
     @RestResource(path = "tag", rel = "tag")
-    fun findByTags(@Param("tag") tag: String, pageable: Pageable): Page<Post>
+    fun findByTags(tag: String, pageable: Pageable): Page<Post>
 
     @RestResource(path = "popular", rel = "popular")
     fun findTop10ByOrderByStatsSharesDesc(pageable: Pageable): List<Post>
