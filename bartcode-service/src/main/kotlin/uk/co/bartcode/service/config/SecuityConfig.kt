f1b05e9b555d2b3ab555d69f4fec6f469cd08ab4
Bartosz Kielczewski <bartosz@kielczewski.eu>
2017-12-03 00:26:59 +0000

    Added Spring Security configuration to prevent modification of content through api endpoints

--- bartcode-service/src/main/kotlin/uk/co/bartcode/service/config/SecuityConfig.kt
+++ bartcode-service/src/main/kotlin/uk/co/bartcode/service/config/SecuityConfig.kt
@@ -1,6 +1,7 @@
 package uk.co.bartcode.service.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -11,6 +12,10 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity) {
         http
                 .authorizeRequests()
+                .antMatchers(HttpMethod.PATCH, "**").fullyAuthenticated()
+                .antMatchers(HttpMethod.PUT, "**").fullyAuthenticated()
+                .antMatchers(HttpMethod.DELETE, "**").fullyAuthenticated()
+                .antMatchers(HttpMethod.POST, "**").fullyAuthenticated()
                 .anyRequest().permitAll()
                 .and()
                 .sessionManagement()
